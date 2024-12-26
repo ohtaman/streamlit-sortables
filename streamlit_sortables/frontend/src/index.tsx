5a3e53117025b88c38d7acae907b3d7fb665e675
ohtaman <ohtamans@gmail.com>
2024-12-27 08:52:14 +0900

    Update dependencies of frontend

--- streamlit_sortables/frontend/src/index.tsx
+++ streamlit_sortables/frontend/src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import SortableComponent from "./SortableComponent"
+import React from "react";
+import ReactDOM from "react-dom";
+import SortableComponent from "./SortableComponent";
 
 ReactDOM.render(
   <React.StrictMode>
     <SortableComponent />
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
