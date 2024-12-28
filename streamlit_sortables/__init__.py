be74270b558996b08274f589406335d3743b8009
Socvest <82335390+Socvest@users.noreply.github.com>
2024-12-28 20:59:41 +0000

    Update __init__.py

--- streamlit_sortables/__init__.py
+++ streamlit_sortables/__init__.py
@@ -100,39 +100,46 @@ def sort_items(items: list[T],  header: Optional[str]=None, multi_containers: bo
 if not _RELEASE:
     import streamlit as st
 
-    # st.title('Sortables')
+    st.title('Sortables')
 
-    # st.write('Sort items in a single container.')
-    # items = ['item1', 'item2', 'item3']
-    # sorted_items = sort_items(items)
-    # st.write(sorted_items)
+    in_line_styles = {
+        'sortable-component-parent-container':{},
+        'sortable-item':{},
+        'sortable-container':{}
+    }
+
+    st.write('Sort items in a single container.')
+    items = ['item1', 'item2', 'item3']
+    
+    sorted_items = sort_items(items, in_line_styles=in_line_styles)
+    st.write(sorted_items)
 
 
-    # st.write('----')
-    # st.write('Sort items in multiple containers.')
-    # items = [
-    #     {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    #     {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
-    # ]
-    # sorted_items = sort_items(items, multi_containers=True)
-    # st.write(sorted_items)
+    st.write('----')
+    st.write('Sort items in multiple containers.')
+    items = [
+        {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
+        {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
+    ]
+    sorted_items = sort_items(items, multi_containers=True, in_line_styles=in_line_styles)
+    st.write(sorted_items)
 
-    # st.write('----')
-    # st.write('Lots of items in a single container.')
-    # items = [
-    #     {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
-    # ]
-    # sorted_items = sort_items(items, multi_containers=True)
-    # st.write(sorted_items)
+    st.write('----')
+    st.write('Lots of items in a single container.')
+    items = [
+        {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
+    ]
+    sorted_items = sort_items(items, multi_containers=True, in_line_styles=in_line_styles)
+    st.write(sorted_items)
 
-    # st.write('----')
-    # st.write('Sort items in multiple containers with vertical direction.')
-    # items = [
-    #     {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    #     {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
-    # ]
-    # sorted_items = sort_items(items, multi_containers=True, direction="vertical")
-    # st.write(sorted_items)
+    st.write('----')
+    st.write('Sort items in multiple containers with vertical direction.')
+    items = [
+        {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
+        {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
+    ]
+    sorted_items = sort_items(items, multi_containers=True, direction="vertical", in_line_styles=in_line_styles)
+    st.write(sorted_items)
 
     st.write('----')
     st.write('Sort items in many containers with vertical direction.')
@@ -144,17 +151,6 @@ if not _RELEASE:
         {'header': 'container5', 'items': ['item13', 'item14', 'item15']},
         {'header': 'container6', 'items': ['item16', 'item17', 'item18']},
     ]
-
-    in_line_styles = {
-        'sortable-component-parent-container':{
-            "backgroundColor":"blue",
-        },
-        'sortable-item':{
-            "backgroundColor":"brown",
-        },
-        'sortable-container':{
-            'backgroundColor':"orange"
-        }
-    }
+    
     sorted_items = sort_items(items, multi_containers=True, direction="vertical", in_line_styles=in_line_styles)
     st.write(sorted_items)
