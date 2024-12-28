8d048b5f12719f9d543ce71537e1517f14bd838f
Socvest <82335390+Socvest@users.noreply.github.com>
2024-12-28 20:40:15 +0000

    Update SortableItem.tsx

--- streamlit_sortables/frontend/src/SortableItem.tsx
+++ streamlit_sortables/frontend/src/SortableItem.tsx
@@ -1,15 +1,18 @@
 import React, {ReactNode, FunctionComponent} from 'react';
 import {useSortable} from '@dnd-kit/sortable';
 import {CSS} from '@dnd-kit/utilities';
-import { style as StyleGlamor } from "glamor";
 
 import './SortableComponent.css'
 
+interface InLineStyles {
+  [key: string]: React.CSSProperties
+}
 
 export interface SortableItemProps {
   id: string,
   isActive?: boolean,
   children?: ReactNode,
+  inLineStyles?: InLineStyles
 }
 
 export const SortableItem: FunctionComponent<SortableItemProps> = ((props) => {
@@ -20,6 +23,7 @@ export const SortableItem: FunctionComponent<SortableItemProps> = ((props) => {
       transform,
       transition,
     } = useSortable({id: props.id});
+    const inLineStyles = props.inLineStyles
 
     const baseStyle: React.CSSProperties = {
       transform: CSS.Transform.toString(transform),
@@ -29,7 +33,7 @@ export const SortableItem: FunctionComponent<SortableItemProps> = ((props) => {
     // Merge baseStyle with inLineStyles["sortable-item"] if it's not empty
     const style: React.CSSProperties = {
       ...baseStyle,
-      ...(Object.keys(inLineStyles["sortable-item"] || {}).length > 0 ? inLineStyles["sortable-item"] : {})
+      ...(Object.keys(inLineStyles?.["sortable-item"] || {}).length > 0 ? inLineStyles?.["sortable-item"] : {})
     };
 
 
